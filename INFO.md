9855a8cb4a3910d1eeb9c7fe253cb39525d3b985
Victor Meyer <victor.reyem@gmail.com>
2016-08-08 12:15:29 +0200

    :tada: Initial commit

---
+++ INFO.md
@@ -0,0 +1,2 @@
+# Emojit
+Command Line Tool to help you write commits using emojis.
---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Emojit.js
+ * A Command Line Tool to help you write commits using emojis.
+ */
+
+'use strict';
+var inquirer = require('inquirer');
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+
+
+var emojis = [
+	{
+		name: '➕  Adding a feature',
+		value: ':heavy_plus_sign:'
+	},
+	{
+		name: '➖  Removing a feature',
+		value: ':heavy_minus_sign:'
+	},
+	{
+		name: '📚  Documentation',
+		value: ':books:'
+	},
+	{
+		name: '🎨  Design',
+		value: ':art:'
+	},
+	{
+		name: '⬆  Upgrading dependencies',
+		value: ':arrow_up:'
+	},
+	{
+		name: '⬇  Upgrading dependencies',
+		value: ':arrow_down:'
+	},
+	{
+		name: '🎉  Initial commit',
+		value: ':tada:'
+	}
+]
+
+inquirer.prompt([
+	{
+		type: 'list',
+		name: 'emoji',
+		message: 'Select emoji:',
+		choices: emojis
+	},
+	{
+		type: 'input',
+		name: 'subject',
+		message: 'Commit message:'
+	}
+]).then(function (answers) {
+	var commit = answers.emoji + ' ' + answers.subject
+	spawn('git', ['commit', '-m ' + commit], {stdio: 'inherit'})
+});
